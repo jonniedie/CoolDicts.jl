75eff51f154e52ca314bbe833b4eb2424de6d400
Jonnie Diegelman <47193959+jonniedie@users.noreply.github.com>
2020-01-23 11:49:32 -0500

    Added documentation for CoolDict. Removed key type parameter.

--- src/types.jl
+++ src/types.jl
@@ -1,12 +1,40 @@
+"""
+    Dict([itr])
 
-struct CoolDict{S,T} <: AbstractDict{S,T}
-    _data::Dict{S,T}
-    CoolDict(args...) = new{Symbol, eltype(args).types[2]}(Dict(args...))
-    CoolDict() = new{Symbol, Any}(Dict())
+    Dict{V}()
+
+Like a normal `Dict`, but cool. Values are settable and gettable through dot notation as
+well as standard `Dict` indexing notation. The only additional restriction is that keys must
+be `Symbolic`.
+
+# Examples
+```julia-repl
+julia> a = CoolDict();
+
+julia> a.a = 1;
+
+julia> a.b = "howdy";
+
+julia> a
+CoolDict{Symbol,Any} with 2 entries:
+  :a => 1
+  :b => "howdy"
+
+julia> a.b
+"howdy"
+```
+
+See also: [`Dict`](@ref).
+"""
+struct CoolDict{V} <: AbstractDict{Symbol,V}
+    _data::Dict{Symbol,V}
+    CoolDict(args...) = new{eltype(args).types[2]}(Dict(args...))
+    CoolDict{V}(args...) where {V} = new{V}(Dict(args...))
+    CoolDict() = new{Any}(Dict())
 end
 
-function Base.show(io::IO, cd::CoolDict{S,T}) where {S,T}
-    println(io, "CoolDict{$S,$T} with $(length(cd)) entries:")
+function Base.show(io::IO, cd::CoolDict{V}) where {V}
+    println(io, "CoolDict{$V} with $(length(cd)) entries:")
     for (key, val) in zip(keys(cd), values(cd))
         println(io, "  :$key => " * decorated_string(val))
     end
@@ -33,6 +61,7 @@ Base.setproperty!(cd::CoolDict, key::Symbol, value) = (cd._data[key] = value)
 Base.keys(cd::CoolDict) = keys(cd._data)
 Base.values(cd::CoolDict) = values(cd._data)
 
+Base.convert(::Type{Dict}, cd::CoolDict) = cd._data
 Base.convert(::Type{Tuple}, cd::CoolDict) = Tuple(cd._data)
-Base.convert(::Type{NamedTuple}, cd::CoolDict) = (; a._data...)
+Base.convert(::Type{NamedTuple}, cd::CoolDict) = (; cd._data...)
 Base.NamedTuple(cd::CoolDict) = convert(NamedTuple, cd)
